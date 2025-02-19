40f376cb2f734a408e2d62e4fdbab39fc0c27779
C.Silva <s1lvax@proton.me>
2025-02-19 19:38:32 +0100

    logout added

--- src/lib/components/dashboard/ProfileOverview.svelte
+++ src/lib/components/dashboard/ProfileOverview.svelte
@@ -26,27 +26,29 @@
 					</div>
 				</div>
 				<div class="mt-5 flex justify-center sm:mt-0">
-					<a
-						href="#"
-						class="flex items-center justify-center gap-2 rounded-md bg-black px-3 py-2 text-sm font-semibold text-white ring-1 shadow-xs dark:bg-white dark:text-black"
-						>Logout <svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24"
-							fill="none"
-							stroke="currentColor"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							width="24"
-							height="24"
-							stroke-width="2"
-						>
-							<path
-								d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2"
-							></path>
-							<path d="M9 12h12l-3 -3"></path>
-							<path d="M18 15l3 -3"></path>
-						</svg>
-					</a>
+					<form method="POST">
+						<button
+							type="submit"
+							class="flex items-center justify-center gap-2 rounded-md bg-black px-3 py-2 text-sm font-semibold text-white ring-1 shadow-xs hover:cursor-pointer dark:bg-white dark:text-black"
+							>Logout <svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="none"
+								stroke="currentColor"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								width="24"
+								height="24"
+								stroke-width="2"
+							>
+								<path
+									d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2"
+								></path>
+								<path d="M9 12h12l-3 -3"></path>
+								<path d="M18 15l3 -3"></path>
+							</svg>
+						</button>
+					</form>
 				</div>
 			</div>
 		</div>
---
+++ src/routes/dashboard/+page.server.ts
@@ -0,0 +1,14 @@
+import { fail, redirect, type Actions } from '@sveltejs/kit';
+import * as auth from '$lib/server/auth';
+
+export const actions: Actions = {
+	default: async (event) => {
+		if (!event.locals.session) {
+			return fail(401);
+		}
+		await auth.invalidateSession(event.locals.session.id);
+		auth.deleteSessionTokenCookie(event);
+
+		return redirect(302, '/');
+	}
+};
